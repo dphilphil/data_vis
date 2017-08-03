3dc265e05ef21035dc9cd70ba0be37a4946aacbf
Phillip Marks <phill.marks@yahoo.co.uk>
2017-08-03 19:18:56 +0100

    Minor syntax changes to improve compatibility

--- isosurf.py
+++ isosurf.py
@@ -5,28 +5,30 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
 #import and sort data
-df = pd.read_csv('fil.csv',sep=',' ,names=['x','y','z'])
+df = pd.read_csv('endconf_toplayer_H.csv',sep=',' ,names=['idx','x','y','z'])
 sorted_df = df.sort_values(['x','y'],ascending=[True,True])
 
 x,y,z = np.array(sorted_df['x']), np.array(sorted_df['y']), np.array(sorted_df['z'])
 
 #return evenly spaced numbers over a specified interval
-xi = np.linspace(x.min(),x.max(),800 )
-yi = np.linspace(y.min(),y.max(),800 )
+xi = np.linspace(min(x),max(x),800)
+yi = np.linspace(min(y),max(y),800)
+
 
 #interpolate over grid
 zi = griddata((x, y), z, (xi[None,:], yi[:,None]), method='cubic')
+
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, projection='3d')
 
 xi_grid, yi_grid = np.meshgrid(xi, yi)
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+ax.set_xlabel('a ($\AA$)')
+ax.set_ylabel('b ($\AA$)')
+ax.set_zlabel('c ($\AA$)')
 
-ax.set_zlim([0.695,0.715])
+ax.set_zlim([20.88,21.00])
 
 surf = ax.plot_surface(xi_grid, yi_grid, zi,
                        linewidth=0.2, cmap='cool',
