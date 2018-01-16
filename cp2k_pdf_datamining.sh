4c8440778ade1d07e002b252809a849dfecbc6c2
Phillip Marks <phill.marks@yahoo.co.uk>
2018-01-16 14:57:40 +0000

    Update cp2k_pdf_datamining.sh

--- cp2k_pdf_datamining.sh
+++ cp2k_pdf_datamining.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-infile="31550.dat"
+infile="Md_xx.xyz"
 #lines in each timestep
-lines_per_step=506
+lines_per_step=510
 
 tot_lines=$(wc -l < $infile)
 iter=$((tot_lines/lines_per_step))
@@ -18,18 +18,22 @@ pre=" -e '"
 for ((i=1; i<=iter; i++))
 do
   #end line of each timestep
-  end_line=$((i*506))
- 
-  #
-  #lines to get
-  l1=$pre$((end_line-303))$comma
-  l3=$((end_line-254))$suffix
-  #
+  end_line=$((i*$lines_per_step))
   
-  list_of_lines+="$l1$l3"
+  #ID
+  l0=$pre$((end_line-509))$comma
+  l1=$((end_line-508))$suffix
+ 
+  #2xNH3
+  l2=$pre$((end_line-7))$comma
+  l3=$((end_line))$suffix
+  #
+
+  
+  list_of_lines+="$l0$l1$l2$l3"
+
 done
 
-list_of_lines+="  $infile >> HonTop.dat"
+list_of_lines+="  $infile >> 2xNH3_Md_xx.xyz"
 
-#execute generated string
 eval $list_of_lines
