d41a38d7ed18ad142164d5342f83976f4a306b02
Phillip Marks <phill.marks@yahoo.co.uk>
2018-01-16 18:08:10 +0000

    Update Automatic_GetAnAtomFromTrajectory.sh

--- Automatic_GetAnAtomFromTrajectory.sh
+++ Automatic_GetAnAtomFromTrajectory.sh
@@ -1,32 +1,45 @@
 in='XX.xyz'
+
+outp_preffix='H501_'
+
 #504 is total number of lines in file. 502+2
 lines_per_step=504
 
 #idx of atom to get
-atom_no=238
-outp_preffix='H238_'
+atom_no=501
 
-###
+####################################
 
 #add header lines
 atom_no=$((atom_no +2))
 
+echo $atom_no
+
 #generate string of \n to append to top of file
-blank_lines=''
+blank_line=''
 
 #atom count
 atom_no=$((lines_per_step - atom_no))
 
-for ((i=1; i<atom_no; i++))
-do
-  blank_lines+='\n'
-done
+#if statement due to failure of awk to correctly add a single \n when atom_no=1. Otherwise awk incorrectly adds \n\n
+
+if [ "$atom_no" != "1" ]
+then
+  for ((i=1; i<iter; i++))
+  do
+    blank_lines+='\n'
+  done
+else
+  blank_lines+='\'
+fi
 
 #tempfile name
 tempf=$in'.tempf'
 
 #make temp file with added blank lines
 sed '1i\'$blank_lines $in >> $tempf
+
+echo $blank_lines
 
 #SEARCH for atoms
 awk 'NR % 504 == 0' $tempf >> $outp_preffix$in
