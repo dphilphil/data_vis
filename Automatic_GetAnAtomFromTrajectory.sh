b6f0dedc9d2db935ed8e124685fda405d2026c59
Phillip Marks <phill.marks@yahoo.co.uk>
2018-01-16 18:15:31 +0000

    renamed an incorrect variable

--- Automatic_GetAnAtomFromTrajectory.sh
+++ Automatic_GetAnAtomFromTrajectory.sh
@@ -1,14 +1,15 @@
 in='XX.xyz'
 
-outp_preffix='H501_'
+outp_preffix='H500_'
 
 #504 is total number of lines in file. 502+2
 lines_per_step=504
 
 #idx of atom to get
-atom_no=501
+atom_no=502
 
 ####################################
+
 
 #add header lines
 atom_no=$((atom_no +2))
@@ -18,14 +19,15 @@ echo $atom_no
 #generate string of \n to append to top of file
 blank_line=''
 
+
 #atom count
 atom_no=$((lines_per_step - atom_no))
 
-#if statement due to failure of awk to correctly add a single \n when atom_no=1. Otherwise awk incorrectly adds \n\n
+#if statement due to failure of awk to correctly add a single \n when atom_no=1. Otherwise awk  incorrectly adds \n\n
 
 if [ "$atom_no" != "1" ]
 then
-  for ((i=1; i<iter; i++))
+  for ((i=1; i<atom_no; i++))
   do
     blank_lines+='\n'
   done
